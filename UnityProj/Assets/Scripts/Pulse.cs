f0fc9140256e4e0e7b67d8f852a2639d51b1d3db
Tanner Pye-Richardson <tannerpyerichardson@gmail.com>
2016-08-10 12:15:53 -0600

    Visual Studio and Script import
    Brought scripts in from inna's laptop

---
+++ UnityProj/Assets/Scripts/Pulse.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour
+{
+    public bool pulseActive;
+
+    void Start()
+    {
+        size = Vector3.zero;
+        pulseActive = false;
+    }
+
+    void Update()
+    {
+        if (pulseActive)
+        {
+            //for (int i = 0; i < 60; i++) 
+            //{
+            // Don't always need a member var for time, just use Time.Time
+            //size.x = Mathf.Sin (Time.time) * i / 60;
+            //size.y = Mathf.Sin (Time.time) * i / 60;
+            //size.z = Mathf.Sin (Time.time) * i / 60;
+
+            size.x = 1;
+            size.y = 1;
+            size.z = 1;
+            pulseActive = false;
+            //}
+            this.gameObject.transform.localScale += size;
+        }
+    }
+
+    //private variables
+    //GameObject obj;
+    Vector3 size;
+    float timer;
+}
---
+++ UnityProj/Assets/Scripts/SimonSays.cs
@@ -0,0 +1,107 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimonSays : MonoBehaviour
+{
+    // Public global variables
+    public int difficulty;
+    public GameObject[] cubeArr;
+
+    void Start()
+    {
+        counter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float time = Time.time;
+
+        // Pattern Creation
+        if (!createdOnce)
+            CreatePattern(cubeArr.Length, difficulty);
+        Debug.Log(pattern.Length + " digit pattern is "
+                                  + pattern[0] + ", "
+                                  + pattern[1] + ", "
+                                  + pattern[2] + ", "
+                                  + pattern[3]);
+        // Player Observation
+        PatternPlayback(difficulty);
+
+        // Player Input
+        UserTurn();
+    }
+
+    /// <summary>
+    /// Create a pattern using the number of gameObjects in the game
+    /// where the pattern is "length" long.
+    /// </summary>
+    /// <param name="reps">Reps.</param>
+    void CreatePattern(int buttonRange, int patternLength)
+    {
+        // create a vector of random ints, within a range
+        pattern = new int[patternLength *= 4];
+
+        // create a pattern as long as "patternLength",
+        // put a number within chosen range into the array
+        for (int i = 0; i < patternLength; i++)
+            pattern[i] = Random.Range(0, buttonRange);
+
+        createdOnce = true;
+    }
+
+    /// <summary>
+    /// Highlight the cubes in the order set by "PatternCreate"
+    /// at desired speed
+    /// </summary>
+    void PatternPlayback(float speed)
+    {
+        //for (int i = 0; i < pattern.Length; i++) 
+        if (counter > pattern.Length) counter = 0;
+        timer += Time.deltaTime;
+        Debug.Log(counter);
+
+        if (timer > 4.0f)
+        {
+            timer = 0;
+            switch (counter)
+            {
+                case 0:
+                    // pulse the    cube of  pattern spot "_"
+                    pulseInstance = cubeArr[pattern[0]].GetComponent<Pulse>();
+                    pulseInstance.pulseActive = true;
+                    counter++;
+                    break;
+                case 1:
+                    pulseInstance = cubeArr[pattern[1]].GetComponent<Pulse>();
+                    pulseInstance.pulseActive = true;
+                    counter++;
+                    break;
+                case 2:
+                    pulseInstance = cubeArr[pattern[2]].GetComponent<Pulse>();
+                    pulseInstance.pulseActive = true;
+                    counter++;
+                    break;
+                case 3:
+                    pulseInstance = cubeArr[pattern[3]].GetComponent<Pulse>();
+                    pulseInstance.pulseActive = true;
+                    counter++;
+                    break;
+            }
+        }
+    }
+
+    void UserTurn()
+    {
+
+    }
+
+    // Private global vars
+    int[] pattern;
+    bool createdOnce = false;
+    Pulse pulseInstance;
+
+    // Private global vars for pattern playback
+    int counter;
+    float timer;
+}
